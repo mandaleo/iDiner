5e058ca95b0728471a513d7a0429596dbf34ff22
Manuel Martinez Gomez <manu.martinez.gomez@gmail.com>
2020-03-24 12:26:14 +0100

    Show list and detail of menu

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -9,13 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(menu) { section in
+          Section(header: Text(section.name)) {
+            ForEach(section.items) { item in
+              ItemRow(item: item)
+            }
+          }
+        }
+      }
+    .navigationBarTitle("Menu")
+    .listStyle(GroupedListStyle())
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
